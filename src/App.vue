5ed39b0382334dc8b9dfad4fc38f90f16194bf0e
Garrett Michael Hensley <garesnap@gmail.com>
2020-07-29 16:30:40 -0400

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,128 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <b-container class="mb-4">
+    <Nav />
+    <b-container class="bg-white">
+      <div class="text-center mb-4">
+        <b-img src="../logo.png" />
+      </div>
+      <div>
+        <b-img src="../banner.png" fluid />
+      </div>
+      <b-container class="pb-3">
+        <div class="mx-5 text-center d-flex flex-column align-items-center">
+          <h2 class="display-4 text-center text-wrap">Food Distribution Event</h2>
+          <p
+            class="lead"
+          >Please join us for our Food Distribution event on August 7th at 2120 Collier Ave Fort Myers, Fl from 1-4pm.</p>
+          <b-list-group class="w-50">
+            <b-list-group-item variant="primary">Event will include:</b-list-group-item>
+            <b-list-group-item>Family Boxes</b-list-group-item>
+            <b-list-group-item>Food & Vegetables</b-list-group-item>
+          </b-list-group>
+        </div>
+      </b-container>
+      <b-container class="green p-4 text-light">
+        <b-row class="nospace">
+          <b-col>
+            <div class="mb-4">
+              <b-img src="../logo2.png" />
+            </div>
+            <p>6111 South Pointe Blvd</p>
+            <p>Fort Myers, Florida 33919</p>
+            <p>239.274.8881</p>
+          </b-col>
+          <b-col>
+            <p class="lead">AFFILIATED MINISTRIES</p>
+            <p>Word Of Life Freeport, NY</p>
+            <p>Stop Hurting Start Healing</p>
+            <p>New Life Dream Centers</p>
+            <p>Kairos University</p>
+            <p>Global Church Fellowship Int'l</p>
+            <p>Covenant with Israel Celebration</p>
+            <p>GasparandMichele.com</p>
+            <p>SWFL Pastors & Leaders Assn.</p>
+          </b-col>
+          <b-col>
+            <div>
+              <p class="lead">WEEKLY CALENDAR</p>
+              <p class="font-weight-bold">Sundays</p>
+              <p>09:00am…Bible Study</p>
+              <p>09:00am…Pre-Service Prayer</p>
+              <p>10:00am…Corporate gathering</p>
+              <p>10:00am…Kids Super Church</p>
+            </div>
+            <div>
+              <p class="font-weight-bold">Tuesdays</p>
+              <p>07:00pm…Sacred Marriage Vine Group</p>
+            </div>
+            <div>
+              <p class="font-weight-bold">Wednesdays</p>
+              <p>07:00pm…Vine Groups</p>
+            </div>
+            <div>
+              <p class="font-weight-bold">Thursdays</p>
+              <p>07:00pm…Kairos University class</p>
+            </div>
+            <div>
+              <p class="font-weight-bold">Fridays</p>
+              <p>07:00pm…Friday Service</p>
+              <p>07:00pm…Kids Super Church</p>
+              <p>07:00pm…Awaken Youth Ministry</p>
+            </div>
+          </b-col>
+          <b-col>
+            <p class="lead">RECENT BLOG POSTS</p>
+            <p class>
+              <em>The Unspoken Addiction July 25, 2020</em>
+            </p>
+            <p>… The post The Unspoken Addiction appeared first on Gaspar & Michele.</p>
+            <p class>
+              <em>Racism in the Church, part 2 July 18, 2020</em>
+            </p>
+            <p>Until we as the body of Christ are willing to join together and look into the mirror of God’s word, we will never see racism done away with. It needs to be eradicated first within us, as Christians, before we can affect the world in which we live. The post Racism in the Church, part […]</p>
+            <p class>
+              <em>Racism in the Church, part 1 July 11, 2020</em>
+            </p>
+            <p>Until we as the body of Christ are willing to join together and look into the mirror of God’s word, we will never see racism done away with. It needs to be eradicated first within us, as Christians, before we can affect the world in which we live. The post Racism in the Church, part […]</p>
+          </b-col>
+        </b-row>
+      </b-container>
+    </b-container>
+  </b-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Nav from "@/components/Nav.vue";
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Nav,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url("https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
+
+html body {
+  background-color: #25705b;
+  font-family: "Lato", sans-serif;
+  font-weight: 400;
+}
+.green {
+  background-color: #1a5445;
+}
+.display-1 {
+  font-weight: 200;
+}
+.no-pad {
+  padding-left: 0 !important;
+  padding-right: 0 !important;
+}
+.nospace p {
+  margin-top: 0.2rem;
+  margin-bottom: 0.2rem;
+}
+.nospace div {
+  margin-top: 0.5em;
 }
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <b-nav class="bg" align="right">
+      <b-nav-item active>Home</b-nav-item>
+      <b-nav-item-dropdown
+        id="my-nav-dropdown"
+        text="About"
+        toggle-class="nav-link-custom"
+        class="text-light"
+        right
+      >
+        <b-dropdown-item>Vision, Mission, & Core Values</b-dropdown-item>
+        <b-dropdown-divider></b-dropdown-divider>
+        <b-dropdown-item>What We Believe</b-dropdown-item>
+        <b-dropdown-divider></b-dropdown-divider>
+        <b-dropdown-item>Senior Leadership Team</b-dropdown-item>
+      </b-nav-item-dropdown>
+      <b-nav-item>Ministries</b-nav-item>
+      <b-nav-item>Events</b-nav-item>
+      <b-nav-item>Live Stream</b-nav-item>
+      <b-nav-item>Online Giving</b-nav-item>
+      <b-nav-item>Blog</b-nav-item>
+      <b-nav-item disabled>Contact Us</b-nav-item>
+    </b-nav>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.bg {
+  background-color: #1a5445;
+}
+.bg a {
+  color: white;
+  font-weight: 400;
+  font-family: "Raleway", sans-serif;
+}
+</style>
